c25134cc49caa4da5a3481448f3249b4b91d8cc8
yeetree <64941543+yeetree@users.noreply.github.com>
2021-01-20 12:53:27 -0600

    Update ultimateformat.js

--- ultimateformat.js
+++ ultimateformat.js
@@ -1,3 +1,9 @@
+function binary(string) {
+    return string.split('').map(function (char) {
+        return char.charCodeAt(0).toString(2);
+    }).join(' ');
+}
+
 function rot13(str) {
   var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
@@ -9,4 +15,63 @@ function rot13(str) {
 function Encode(data) {
   var encode = btoa(btoa(btoa(btoa(btoa(data)))));
   var encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = binary(encode2);
+  
+  encode2 = btoa(btoa(btoa(btoa(btoa(data)))));
+  encode = rot13(encode2);
+  encode2 = btoa(btoa(btoa(btoa(btoa(encode)))));
+  encode = rot13(encode2);
+  encode2 = btoa(btoa(btoa(btoa(btoa(encode)))));
+  encode = rot13(encode2);
+  encode2 = btoa(btoa(btoa(btoa(btoa(encode)))));
+  encode = rot13(encode2);
+  encode2 = btoa(btoa(btoa(btoa(btoa(encode)))));
+  encode = rot13(encode2);
+  encode2 = binary(encode);
+  
+  encode = btoa(btoa(btoa(btoa(btoa(data)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = binary(encode2);
+  
+  encode2 = btoa(btoa(btoa(btoa(btoa(data)))));
+  encode = rot13(encode2);
+  encode2 = btoa(btoa(btoa(btoa(btoa(encode)))));
+  encode = rot13(encode2);
+  encode2 = btoa(btoa(btoa(btoa(btoa(encode)))));
+  encode = rot13(encode2);
+  encode2 = btoa(btoa(btoa(btoa(btoa(encode)))));
+  encode = rot13(encode2);
+  encode2 = btoa(btoa(btoa(btoa(btoa(encode)))));
+  encode = rot13(encode2);
+  encode2 = binary(encode);
+  
+  encode = btoa(btoa(btoa(btoa(btoa(data)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = btoa(btoa(btoa(btoa(btoa(encode2)))));
+  encode2 = rot13(encode);
+  encode = binary(encode2);
+  
+  return encode;
 }
