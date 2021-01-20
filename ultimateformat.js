f031bd25e48674379e6fd21bce8ca69f11bddea9
yeetree <64941543+yeetree@users.noreply.github.com>
2021-01-20 12:34:41 -0600

    Create ultimateformat.js

---
+++ ultimateformat.js
@@ -0,0 +1,12 @@
+function rot13(str) {
+  var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  var index     = x => input.indexOf(x);
+  var translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
+}
+
+function Encode(data) {
+  var encode = btoa(btoa(btoa(btoa(btoa(data)))));
+  var encode2 = rot13(encode);
+}
